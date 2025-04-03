b3cee2c650d3b8ade232db69d6487564c6aa1c72
nadir675 <kerimzadenadir6@gmail.com>
2025-04-03 22:02:43 +0400

    feat(task): add task function

---
+++ index.js
@@ -0,0 +1,18 @@
+const clickMeBtn = document.getElementById("click-me -btn")
+const input = document.getElementById("input")
+
+
+let task = [];
+
+document.addEventListener("DOMContentLoaded",() => {
+
+    clickMeBtn.addEventListener('click', () => {
+        tasks.push(input.value)
+        
+        const task = document.createElement("p")
+        task.innerText -input.value;
+        list.appendchild(task)
+    })
+    
+
+})
